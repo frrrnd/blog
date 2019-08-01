07ea70b4271a4162201dad1642f2412723d13e80
Fernando Soares <dotfernando@gmail.com>
2019-08-01 13:40:28 -0300

    chore: test force push on deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 # we need the || true, as sometimes you do not have any content changes
 # and git woundn't commit and you don't want to break the CI because of that
 git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
-git push
+git push origin master --force
 
 cd ..
 rm -rf deployment
